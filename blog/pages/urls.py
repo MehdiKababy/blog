81166b73b4969b1fd9dbc80957200e4862a1b1c2
unknown <mehdikababy@gmail.com>
2025-11-10 18:59:45 +0330

    add create and edit post for members

--- blog/pages/urls.py
+++ blog/pages/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", home_page, name="home page"),
     path("posts/", all_posts, name="all posts"),
     path("post/<int:pk>", select_post, name="select post"),
+    path('create_post/', create_post, name="create post")
 ]
--- blog/pages/views.py
+++ blog/pages/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Post
+from .forms import PostForm
 
 
 def home_page(request):
@@ -13,4 +14,20 @@ def all_posts(request):
 
 def select_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, "./select_post/select_post.html", {"post": post})
+    edit_post = PostForm(instance=post)
+    if request.method == "POST":
+        edit_post = PostForm(request.POST, instance=post)
+        if edit_post.is_valid():
+            edit_post.save()
+            return redirect("select post", pk=pk)
+    else:
+        return render(request, "./select_post/select_post.html", {"post": post, 'form': edit_post})
+def create_post(request):
+    new_post = PostForm()
+    if request.method == "POST":
+        new_post = PostForm(request.POST)
+        if new_post.is_valid():
+            new_post.save()
+            return redirect("all posts")
+    else:
+        return render(request, "./create_post/create_post.html", {'form': new_post})
